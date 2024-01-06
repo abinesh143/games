596b9151c6cb92d5dcd044d953532e14e0e32c15
abinesh143 <abi.posbyz@gmail.com>
2024-01-06 22:07:35 +0530

    install pwa

--- smiley_games/src/app/page.js
+++ smiley_games/src/app/page.js
@@ -7,41 +7,70 @@ import Slider from "@/components/Slider";
 import GameSection from "@/components/GameSection";
 
 export default function Home() {
+  const [prompt, setPrompt] = useState(null);
   const gameConfig = [
     {
       name: "Play Arcade Games",
       buttonClass: "btn-green-moon",
-      slug: "Arcade"
+      slug: "Arcade",
     },
     {
       name: "Play Action Games",
       buttonClass: "btn-dark-moon",
-      slug: "Action"
+      slug: "Action",
     },
     {
       name: "Sports and Racing",
       buttonClass: "btn-funky-moon",
-      slug: "Sports & Racing"
+      slug: "Sports & Racing",
     },
     {
       name: "Play Strategy Games",
       buttonClass: "btn-purple-moon",
-      slug: "Strategy"
+      slug: "Strategy",
     },
     {
       name: "Puzzle and Logic",
       buttonClass: "btn-cool-blues",
-      slug: "Puzzle & Logic"
+      slug: "Puzzle & Logic",
     },
     {
       name: "Play Adventure Games",
       buttonClass: "btn-orange-moon",
-      slug: "Adventure"
+      slug: "Adventure",
     },
   ];
 
+  const showInstallPrompt = () => {
+    if (prompt) {
+      prompt.prompt()
+    }
+  };
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setPrompt(event);
+
+      if (!window.matchMedia("display-mode: standalone").matches) {
+        setTimeout(() => {
+          showInstallPrompt();
+        }, 5000);
+      } 
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
   }, []);
 
   return (
